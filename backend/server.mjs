d1552d5cd02ef28fa7e46ccce530b1d03924405e
Sara Moon <saraastaeva1@gmail.com>
2026-03-05 19:23:19 +0100

    fix static files

--- backend/server.mjs
+++ backend/server.mjs
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { GoogleGenAI } from "@google/genai";
+import { fileURLToPath } from "url";
+import path from "path";
 
 dotenv.config();
 
@@ -9,6 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "../frontend")));
 const PORT = process.env.PORT || 3000;
 const API_KEY = process.env.GEMINI_API_KEY;
 const UNSPLASH_KEY = process.env.UNSPLASH_ACCESS_KEY;
